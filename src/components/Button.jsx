d7fb14a3df38f8128268e6cdae5d275062973d75
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-25 22:51:52 +0530

    Refactor Button component 🥶

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
+import clsx from "clsx";
 import PropTypes from "prop-types";
 
 const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
   return (
     <button
       id={id}
-      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black
-        ${containerClass}`}
+      className={clsx(
+        "group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black",
+        containerClass
+      )}
     >
       {leftIcon}
 
