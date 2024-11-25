678df7ceec9cdad4f0f61f9c2dfb0332fb8a3afb
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-25 21:18:55 +0530

    Added Hero component to app 🐳

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Hero from "./components/Hero"
+
 const App = () => {
   return (
-    <div>
-      Valorant Landing Page
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <section className="z-0 min-h-screen bg-blue-500" />
+    </main>
   )
 }
 
