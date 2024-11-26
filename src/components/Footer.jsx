265ae49579660d1179f5213783fa45b8b6c06f43
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-26 18:50:29 +0530

    Add Footer component 🦊

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import { FaEnvelope, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const socialLinks = [
+    {href: 'https://www.github.com/ramxcodes', icon: <FaGithub />},
+    {href: 'https://www.linkedin.com/in/ramxcodes', icon: <FaLinkedin />},
+    {href: 'mailto:ramkrishnaswarnkar99@gmail.com' , icon: <FaEnvelope />},
+    {href: 'https://www.twitter.com/ramxcodes', icon: <FaTwitter />},
+]
+
+const Footer = () => {
+    return (
+      <footer className="w-screen bg-[#5542ff] py-4 text-black">
+        <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+          <a href="https://www.github.com/ramxcodes" className="text-center text-sm font-light md:text-left">
+            ©Ramxcodes 2024. All rights reserved
+          </a>
+  
+          <div className="flex justify-center gap-4  md:justify-start">
+            {socialLinks.map((link, index) => (
+              <a
+                key={index}
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+              >
+                {link.icon}
+              </a>
+            ))}
+          </div>
+  
+          <a
+            href="#privacy-policy"
+            className="text-center text-sm font-light hover:underline md:text-right"
+          >
+            Privacy Policy
+          </a>
+        </div>
+      </footer>
+    );
+  };
+  
+
+export default Footer;
